70eeb6669898229e2b1b997e95b53214e9004713
Grafcet.Online <77163024+grafcetonline@users.noreply.github.com>
2022-01-30 05:13:15 +0100

    Update getChildIndex.ts
    update

--- src/lib/figmaUtils/search/getChildIndex.ts
+++ src/lib/figmaUtils/search/getChildIndex.ts
@@ -1,13 +1,8 @@
-function getChildIndex(child: SceneNode, context?) {
+function getChildIndex(child: SceneNode) {
 
     let parent = child.parent;
     let siblings = parent.children;
     let index = siblings.indexOf(child) + 1;
     return index;
 
-}
-
-go.getChildIndex = function() {
-    let sel = getSelection();
-    return getChildIndex(sel);
 }
